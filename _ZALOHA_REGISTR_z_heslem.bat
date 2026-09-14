6f8bb88900d6fb4acd5bf3600d69c15c891d5e76
Martin Pávek <pavek_martin@seznam.cz>
2026-09-14 13:05:09 +0200

    Add files via upload

---
+++ _ZALOHA_REGISTR_z_heslem.bat
@@ -0,0 +1,67 @@
+@echo off
+title _ZALOHA_REGISTR.bat
+
+REM zalohuje celej systemovej registr exportem do souboru *.reg a naslednym
+REM zapakovani v "RAR" ( asi 20x mensi velikost, uspora mista )
+REM pak po sobe uklidi, take maze ve se starsim datumem, vytvorene drive
+REM soubor je potreba spustit z Admin prvavama tzn. kliknout na nej pravou mysi
+REM a pak vybrat polozku nahore "Spustit jako spravce" ( malej stit u toho, ikona )
+ 
+:check_Permissions
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto main
+) else (
+    echo CHYBA: Spuste skript jako administrator!
+    echo kliknete na nej pravou mysi a pak vyberte polozku [Spustit jako spravce]
+    pause
+    goto end
+)
+
+:main
+REM vytvori dnesni datum jako parametr 2
+set dat=%DATE%
+REM echo %dat%
+set d=%dat:~0,2%
+REM echo %d%
+set m=%dat:~3,2%
+REM echo %m%
+set r=%dat:~6,4%
+REM echo %r%
+set dnes=%d%%m%%r%
+REM echo %dnes%
+set file_reg=%dnes%_registr.reg
+REM echo %file_reg%
+set file_rar=%dnes%_registr.rar
+REM echo %file_rar%
+
+REM byl problem v rezimu Admin z nastavenim promenni pomoci prikazu set
+REM jako nahradu za retezec "C:\Users\DELL\Documents\zaloha\"
+REM tady mapriklad -  set cesta=C:\Users\DELL\Documents\zaloha\
+REM a nasledne pak - del %cesta%*_registr.reg
+REM dokoce to vypisovalo ze hodnota %cesta% je C:\Windows\system32\ !!!
+REM takze toto zde nepouzivat !
+
+del "C:\Users\DELL\Documents\zaloha\*_registr.reg"
+del "C:\Users\DELL\Documents\zaloha\*_registr.rar"
+REM napred smaze vse stary ( kdyby bylo )
+
+echo probiha export...
+regedit /E "C:\Users\DELL\Documents\zaloha\%file_reg%"
+REM parametr "/E" bude asi neco jako Export
+sleep 1
+
+rar a -m5 -pheslo "C:\Users\DELL\Documents\zaloha\%file_rar%" "C:\Users\DELL\Documents\zaloha\%file_reg%"
+REM tady pridano ze parametr "-p" je password a heslo je "heslo"
+REM takze arhiv bude chranem proti rozbaleni timto heslem, take je to nalepany na sobe "-pheslo"
+REM parametr a=add ; -m5 = maximalni komprese ( viz. manual )
+sleep 1
+
+REM smazani pres navratovy kod, smaze soubor *.reg pouze paklize test souboru *.rar dopadl uspesne
+REM otestovano, funguje jak ma viz. screenshoty
+rar t -pheslo C:\Users\DELL\Documents\zaloha\%file_rar% && del C:\Users\DELL\Documents\zaloha\%file_reg%
+REM samozdrejme ze heslo se musi pouzit potom pri kontole, bez nej by dovnitr nevidel
+REM takze pro toho kdo by chtel, tak jeste neco malo navic
+pause
+:end
+
